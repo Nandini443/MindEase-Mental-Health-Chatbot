f0330475329885d5c6c1485d1fff1eb1190627ba
PAPISETTI NAGA NANDINI <nandiniroyal717@gmail.com>
2026-03-02 20:42:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,10 @@
-from flask import Flask, render_template, request, session
-from chatbot import get_response
-import secrets
+from flask import Flask
 
 app = Flask(__name__)
-app.secret_key = secrets.token_hex(16)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
-    if "chat" not in session:
-        session["chat"] = []
-
-    if request.method == "POST":
-        user_message = request.form.get("message")
-
-        if user_message:
-            bot_reply = get_response(user_message)
-
-            session["chat"].append({
-                "user": user_message,
-                "bot": bot_reply
-            })
-
-    return render_template("index.html", chat=session["chat"])
+    return "MindEase is live ✅"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
